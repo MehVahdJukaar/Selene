167e07933149608dfa9458ecc006aa6fa487fab6
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-10-24 19:44:02 +0200

    some helper

--- common/src/main/java/net/mehvahdjukaar/moonlight/api/client/util/VertexUtil.java
+++ common/src/main/java/net/mehvahdjukaar/moonlight/api/client/util/VertexUtil.java
@@ -2,8 +2,15 @@ package net.mehvahdjukaar.moonlight.api.client.util;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.Direction;
 import net.minecraft.util.FastColor;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.state.BlockState;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
 import org.joml.Vector3f;
 
 public class VertexUtil {
@@ -166,4 +173,16 @@ public class VertexUtil {
     public static int lightV(int light) {
         return light >> 16 & '\uffff';
     }
+    private static final Direction[] DIRS = new Direction[]{
+            Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST, null
+    };
+
+    public static List<BakedQuad> getAllModelQuads(BakedModel model, BlockState state, RandomSource rand) {
+        List<BakedQuad> allQuads = new ArrayList<>();
+        for (var d : DIRS) {
+            allQuads.addAll(model.getQuads(state, d, rand));
+        }
+        return allQuads;
+    }
+
 }
--- common/src/main/java/net/mehvahdjukaar/moonlight/api/resources/pack/DynamicResourcePack.java
+++ common/src/main/java/net/mehvahdjukaar/moonlight/api/resources/pack/DynamicResourcePack.java
@@ -319,7 +319,6 @@ public abstract class DynamicResourcePack implements PackResources {
     protected void clearNonStatic() {
         if (!this.needsClearingNonStatic) return;
         this.needsClearingNonStatic = false;
-        Stopwatch watch = Stopwatch.createStarted();
         boolean mf = MODERN_FIX && getPackType() == PackType.CLIENT_RESOURCES;
         for (var r : this.resources.keySet()) {
             if (mf && modernFixHack(r.getPath())) {
@@ -346,8 +345,6 @@ public abstract class DynamicResourcePack implements PackResources {
         for (var s : staticResources) {
             this.searchTrie.insert(s);
         }
-        Moonlight.LOGGER.info("Cleared non-static resources for pack {} in: {} ms", this.resourcePackName,
-                watch.elapsed().toMillis());
     }
 
     // Called after each reload
